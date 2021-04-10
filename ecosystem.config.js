9c373a15940e651c90e394eabb55c6f9ed189167
Marcos Molina <marcosmol204@gmail.com>
2021-04-10 15:25:19 +0300

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,7 @@ module.exports = {
   apps: [{
     name: 'app',
     script: './src/index.js',
-    instances: 'max',
-    env: {
-      NODE_ENV: 'development',
-    },
-    env_production: {
-      NODE_ENV: 'production',
-    },
+    instances: 4,
+    watch: true,
   }],
 };
