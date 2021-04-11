f3f1d87950fc828b62361cb6066d8d9897c02434
Marcos Molina <marcosmol204@gmail.com>
2021-04-11 20:40:02 +0300

    test

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', req.headers.origin);
   res.set('Access-Control-Allow-Credentials', 'true');
   res.set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, mode, credentials');
   next();
 });
 app.use(cookieParser());
